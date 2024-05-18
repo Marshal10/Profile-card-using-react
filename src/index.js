57a6c86bb494f5e9a9f21049540e4f38ffe3e3ad
Marshal10 <marshalmaleane123@gmail.com>
2024-05-18 15:05:27 +0530

    Avatar and info components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,28 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="card">
+      <Avatar />
+      <ProfileInfo />
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="images/profile.jpg" alt="profile_img"></img>;
+}
+
+function ProfileInfo() {
+  return (
+    <div className="info">
+      <p className="name">Marshal Malean</p>
+      <p className="work">Front-end developer from Bengaluru</p>
+      <p>
+        📌<span className="location">Bengaluru,India</span>
+      </p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
